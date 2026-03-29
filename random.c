5bf4584a6361db7ab23ed26013c8315815d94322
Lahee <huhu4886@naver.com>
2026-03-29 18:46:34 +0900

    pwnable.kr - random
    Add functionality to calculate and display key value using seed.

--- random.c
+++ random.c
@@ -1,3 +1,5 @@
+//문제 정답인 key값을 구하는 함수
+//출력 key값을 그대로 복사해 random파일의 입력으로 넣으면 된다!
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,6 +9,8 @@ unsigned int exploit = 0xcafebabe;
 
 unsigned int key = seed ^ exploit;
 
+  //시드값 확인
 printf("seed : %d \n", seed);
+  //key값 출력
 printf("key : %d", key);
 }
